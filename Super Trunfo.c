1c4be6ba389e1059296d937720b428bc913e5802
AndreGR-ti <deresk8@gmail.com>
2025-03-15 20:06:04 -0300

    Add files via upload

---
+++ Super Trunfo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main(){
+
+    int Carta_01;
+
+    char Estado;
+    char Codigo[(5)];
+    char Cidade[20];
+    int Populacao;
+    float Area;
+    float Pib;
+    int Pontos_turisticos;
+
+    printf("Digite o estado: \n");
+    scanf("%s", &Estado);
+
+    printf("Digite o codigo: \n");
+    scanf("%s", &Codigo);
+
+    printf("Digite a cidade: \n");
+    scanf("%s", &Cidade);
+
+    printf("Digite a populacao: \n");
+    scanf("%d", &Populacao);
+
+    printf("Digite a area: \n");
+    scanf("%f", &Area);
+
+    printf("Digite o Pib: \n");
+    scanf("%f", &Pib);
+
+    printf("Digite o numero de pontos turisticos: \n");
+    scanf("%d", &Pontos_turisticos);
+
+    printf("Carta_01\n");
+    printf("Estado: %s\n", &Estado);
+    printf("Codigo: %s\n", &Codigo);
+    printf("Cidade: %s\n", &Cidade);
+    printf("Populacao: %d\n", Populacao);
+    printf("Area: %f\n", Area);
+    printf("Pib: %f\n", Pib);
+    printf("Pontos_turisticos: %d\n", Pontos_turisticos);
+
+
+    return 0;
+
+
+}
